71993474ad1bcde87784ee8dc0b4320b49954fa3
Anthony Anthony <anstronott@gmail.com>
2022-02-23 00:43:58 +0700

    Added Slider

--- app.js
+++ app.js
@@ -12,3 +12,46 @@ navButt.addEventListener("click", () => {
 		displayed = false;
 	}
 });
+
+// Sliding
+let slides = $(".classes-promos-img-slides");
+let textSlides = $(".classes-promos-img-text-slides");
+let slideSize = 3;
+let currIdx = 0;
+
+slides.hide();
+textSlides.hide();
+slides.eq(currIdx).show();
+textSlides.eq(currIdx).show();
+
+$(".classes-promos-img-left-btn").click(function () {
+	slides.eq(currIdx).animate({}, 1000, function () {
+		slides.eq(currIdx).hide();
+		textSlides.eq(currIdx).hide();
+	});
+	if (currIdx > 0) {
+		currIdx--;
+	} else {
+		currIdx = 2;
+	}
+	slides.eq(currIdx).animate({}, 1000, function () {
+		slides.eq(currIdx).show();
+		textSlides.eq(currIdx).show();
+	});
+});
+
+$(".classes-promos-img-right-btn").click(function () {
+	slides.eq(currIdx).animate({}, 1000, function () {
+		slides.eq(currIdx).hide();
+		textSlides.eq(currIdx).hide();
+	});
+	if (currIdx < slideSize - 1) {
+		currIdx++;
+	} else {
+		currIdx = 0;
+	}
+	slides.eq(currIdx).animate({}, 1000, function () {
+		slides.eq(currIdx).show();
+		textSlides.eq(currIdx).show();
+	});
+});
