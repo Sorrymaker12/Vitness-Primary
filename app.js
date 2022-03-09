76f14b78bd2180eaece104e347e4db6dd3ef23eb
Anthony Anthony <anstronott@gmail.com>
2022-03-09 22:54:46 +0700

    screensaver

--- app.js
+++ app.js
@@ -69,3 +69,58 @@ let blogbtn = document.getElementById("blog-btn");
 blogbtn.addEventListener("click", () => {
 	window.location.href = "blog.html";
 });
+
+// Screensaver
+const canvas = document.getElementById("mc");
+canvas.width = window.innerWidth - 64;
+canvas.height = window.innerHeight - 32;
+const ctx = canvas.getContext("2d");
+let img = new Image();
+img.src = "./Images/ss.png";
+
+let x = 0;
+let y = 0;
+let x1 = 1;
+let y1 = 1;
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	ctx.drawImage(img, x, y);
+
+	if (x <= 0) {
+		x1 = 2;
+	} else if (x >= canvas.width - img.width) {
+		x1 = -2;
+	}
+
+	if (y <= 0) {
+		y1 = 2;
+	} else if (y >= canvas.height - img.height) {
+		y1 = -2;
+	}
+
+	x += x1;
+	y += y1;
+
+	window.requestAnimationFrame(draw);
+}
+
+window.requestAnimationFrame(draw);
+
+// Check Idle and Show/Hide Screensaver
+$("#mc").hide();
+var to;
+var isOn = false;
+var idle = 5000;
+
+$(document).mousemove(function () {
+	clearTimeout(to);
+	if (isOn) {
+		$("#mc").hide();
+		isOn = false;
+	}
+	to = setTimeout(function () {
+		$("#mc").show();
+		isOn = true;
+	}, 3000);
+});
