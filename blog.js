2e7e7a60cbd43db8500e79652d0fdda6d291a11c
Anthony Anthony <anstronott@gmail.com>
2022-03-10 10:53:02 +0700

    added desc on blogs

--- blog.js
+++ blog.js
@@ -4,30 +4,35 @@ const blog = [
 		blogTitle: "Get Started With Fitness",
 		datePosted: "2022-01-01",
 		category: "Fitness",
+		description: "All about fitness",
 	},
 	{
 		id: 2,
 		blogTitle: "How to Build A Healthy Lifestyle",
 		datePosted: "2022-01-01",
 		category: "Lifestyle",
+		description: "All about Healthy Lifestyle",
 	},
 	{
 		id: 3,
 		blogTitle: "Get Started With Keto Diet",
 		datePosted: "2022-01-01",
 		category: "Diet",
+		description: "All about Keto Diet",
 	},
 	{
 		id: 4,
 		blogTitle: "Get Started With Paleo Diet",
 		datePosted: "2022-01-01",
 		category: "Diet",
+		description: "All about Paleo Diet",
 	},
 	{
 		id: 5,
 		blogTitle: "Get Started With Mediteranean Diet",
 		datePosted: "2022-01-01",
 		category: "Diet",
+		description: "All about Mediteranean Diet",
 	},
 ];
 
@@ -43,6 +48,7 @@ blog.forEach(function createBlog(item) {
         <img src="./Images/blog-img-${item.id}.jpg" alt="" class="blog-img" />
         <p> Date Posted : ${item.datePosted}</p>
         <p> Category : ${item.category}</p>
+		<p> Description : ${item.description}</p>
     </a>
     </div>
     `);
